73903cbea6d7f717ff03321bbf47a9abf3df1014
loischen68 <a8283626@gmail.com>
2025-08-07 17:34:26 +0800

    chore: init prettier, prettier-plugin-tailwindcss

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,27 +1,24 @@
-import type { StorybookConfig } from '@storybook/react-vite';
-import { resolve } from 'path';
+import type { StorybookConfig } from "@storybook/react-vite";
+import { resolve } from "path";
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
     "@chromatic-com/storybook",
     "@storybook/addon-docs",
     "@storybook/addon-onboarding",
     "@storybook/addon-a11y",
-    "@storybook/addon-vitest"
+    "@storybook/addon-vitest",
   ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
   },
-  viteFinal: async (config) => {
+  viteFinal: async config => {
     if (config.resolve) {
       config.resolve.alias = {
         ...config.resolve.alias,
-        '@': resolve(__dirname, '../src'),
+        "@": resolve(__dirname, "../src"),
       };
     }
 
@@ -29,8 +26,8 @@ const config: StorybookConfig = {
     if (!config.plugins) {
       config.plugins = [];
     }
-    
-    const tailwindcss = (await import('@tailwindcss/vite')).default;
+
+    const tailwindcss = (await import("@tailwindcss/vite")).default;
     config.plugins.push(tailwindcss());
 
     return config;
